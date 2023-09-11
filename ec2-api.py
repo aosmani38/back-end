816b9edd466d897f91c4e91ac1c0d2bcf1bcc210
Ali Osmani <77931122+aosmani38@users.noreply.github.com>
2023-09-11 13:39:17 -0700

    Update ec2-api.py

--- ec2-api.py
+++ ec2-api.py
@@ -59,6 +59,7 @@ def predict():
         # Perform inference
         model = ImageToWordModel(model_path=configs.model_path, char_list=configs.vocab)
         prediction_text = model.predict(image_np)
+        print("prediction------->")
         print(prediction_text)
 
         # Return the prediction result as json object
