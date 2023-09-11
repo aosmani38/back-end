6503ff7fdb38e0bfba2b9f98afa6207c55bc14be
Ali Osmani <77931122+aosmani38@users.noreply.github.com>
2023-09-11 11:34:24 -0700

    Update ec2-api.py

--- ec2-api.py
+++ ec2-api.py
@@ -64,5 +64,5 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)})
 
-if __name__ == '__main__':
-    app.run(debug=False)
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0')
